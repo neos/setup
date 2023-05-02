d637dfcb60a2fe050b3d772a8c56932a41ac2901
Jon Uhlmann <account@uhlmann.pro>
2023-05-02 22:44:27 +0200

    TASK: Update Build stack

--- Resources/Private/SetupDashboard/tailwind.config.js
+++ Resources/Private/SetupDashboard/tailwind.config.js
@@ -1,8 +1,4 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ["./src/**/*.{ts,html}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+	content: ["./src/**/*.{js,html}"],
 }

e7905079ecb9e65aae643de0e1cb8fbd5f5d464f
mhsdesign <85400359+mhsdesign@users.noreply.github.com>
2023-05-02 20:09:06 +0200

    TASK: DoctrineHealthcheck

---
+++ Classes/Infrastructure/Healthcheck/DoctrineHealthcheck.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Neos\Setup\Infrastructure\Healthcheck;
+
+use Neos\Flow\Persistence\Doctrine\Service as DoctrineService;
+use Neos\Setup\Domain\Health;
+use Neos\Setup\Domain\HealthcheckInterface;
+use Neos\Setup\Domain\Status;
+
+class DoctrineHealthcheck implements HealthcheckInterface
+{
+    public function __construct(
+        private readonly DoctrineService $doctrineService
+    ) {
+    }
+
+    public function getTitle(): string
+    {
+        return 'Doctrine';
+    }
+
+    public function execute(): Health
+    {
+        [
+            'new' => $newMigrationCount,
+            'executed' => $executedMigrationCount,
+            'available' => $availableMigrationCount
+        ] = $this->doctrineService->getMigrationStatus();
+
+        if ($executedMigrationCount === 0 && $availableMigrationCount > 0) {
+            return new Health(
+                <<<'MSG'
+                No doctrine migrations have been executed. Please run <code>./flow doctrine:migrate</code>
+                MSG,
+                Status::ERROR
+            );
+        }
+
+        if ($newMigrationCount > 0) {
+            return new Health(
+                <<<'MSG'
+                Some doctrine migrations have yet to be executed. Please run <code>./flow doctrine:migrate</code>
+                MSG,
+                Status::WARNING
+            );
+        }
+
+        return new Health(
+            <<<'MSG'
+            All doctrine migrations have been executed.
+            MSG,
+            Status::OK
+        );
+    }
+}

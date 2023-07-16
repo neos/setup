b740622c43fae17b92262f9c5923f86327cb0f89
mhsdesign <85400359+mhsdesign@users.noreply.github.com>
2023-07-16 10:43:17 +0200

    TASK: Remove dublicated checks which are enforced by the bootstrap and never run

--- Classes/Infrastructure/Healthcheck/BasicRequirementsHealthcheck.php
+++ Classes/Infrastructure/Healthcheck/BasicRequirementsHealthcheck.php
@@ -34,28 +34,22 @@ class BasicRequirementsHealthcheck implements EarlyBootTimeHealthcheckInterface
         return 'Basic system requirements';
     }
 
+    /**
+     * Additional checks to {@see Bootstrap::ensureRequiredEnvironment()}
+     */
     public function execute(HealthcheckEnvironment $environment): Health
     {
         $this->environment = $environment;
         try {
-            $this->checkDirectorySeparator();
             $this->checkPhpBinaryVersion();
             $this->requiredFunctionsAvailable();
             $this->checkFilePermissions();
             $this->checkSessionAutostart();
-            $this->checkReflectionStatus();
         } catch (HealthcheckFailedError $error) {
             return new Health($error->getMessage(), Status::ERROR);
         }
 
         return new Health('All basic requirements are fullfilled.', Status::OK);
-    }
-
-    private function checkDirectorySeparator(): void
-    {
-        if (DIRECTORY_SEPARATOR !== '/' && PHP_WINDOWS_VERSION_MAJOR < 6) {
-            throw new HealthcheckFailedError('Flow does not support Windows versions older than Windows Vista or Windows Server 2008, because they lack proper support for symbolic links.');
-        }
     }
 
     private function checkFilePermissions(): void
@@ -98,20 +92,6 @@ class BasicRequirementsHealthcheck implements EarlyBootTimeHealthcheckInterface
     {
         if (ini_get('session.auto_start')) {
             throw new HealthcheckFailedError('"session.auto_start" is enabled in your php.ini. This is not supported and will cause problems.');
-        }
-    }
-
-    /**
-     * This doc-comment is used to check if doc comments are available.
-     * DO NOT REMOVE
-     */
-    private function checkReflectionStatus(): void
-    {
-        $method = new \ReflectionMethod(__CLASS__, __FUNCTION__);
-        $docComment = $method->getDocComment();
-
-        if ($docComment === false || $docComment === '') {
-            throw new HealthcheckFailedError('Reflection of doc comments is not supported by your PHP setup. Please check if you have installed an accelerator which removes doc comments.');
         }
     }
 
